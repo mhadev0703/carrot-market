08e45f27bc59125afd4f7b8f529d63863067c65f
mhadev0703 <minchulha0703@gmail.com>
2024-08-02 17:45:17 -0400

    Apply ring variable and responsive modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex 
-    items-center justify-center p-5 ">
+    <main className="bg-gray-100 sm:bg-red-100
+    md:bg-green-100 h-screen flex items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 
       rounded-3xl w-full max-w-screen-sm flex
-      flex-col gap-2 ">
+      flex-col md:flex-row gap-2 ">
         <input 
           className="w-full rounded-full h-10
-          bg-gray-200 pl-5 outline-none ring"
+          bg-gray-200 pl-5 outline-none ring
+          ring-transparent focus:ring-orange-500
+          focus:ring-offest-2 transition-shadow"
           type="text"
           placeholder="Search here..." 
         />
         <button className="bg-black bg-opacity-50 text-white py-2
         rounded-full active:scale-90 focus:scale-90
-        transition-transform font-medium outline-none">
+        transition-transform font-medium 
+        outline-none md:px-10">
           Search
         </button>
       </div>
