36b2ce98ccb8eb83a11b6c71c4d593d95fbe4a41
mhadev0703 <minchulha0703@gmail.com>
2024-08-01 23:59:06 -0400

    Implement search bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,20 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex 
     items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 
-      rounded-3xl w-full max-w-screen-sm "></div>
+      rounded-3xl w-full max-w-screen-sm flex
+      flex-col gap-2 ">
+        <input 
+          className="w-full rounded-full h-10
+          bg-gray-200 pl-5 outline-none ring"
+          type="text"
+          placeholder="Search here..." 
+        />
+        <button className="bg-black bg-opacity-50 text-white py-2
+        rounded-full active:scale-90 focus:scale-90
+        transition-transform font-medium outline-none">
+          Search
+        </button>
+      </div>
     </main>
   );
 }
