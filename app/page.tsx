de5ac5628ac22face8c9ecf79bb1395661ad03d0
mhadev0703 <minchulha0703@gmail.com>
2024-08-05 02:13:54 -0400

    Add invalid condition with has variant

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,23 @@ export default function Home() {
     md:bg-green-100 h-screen flex items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 
       rounded-3xl w-full max-w-screen-sm flex
-      flex-col md:flex-row gap-2 ">
-        <input 
-          className="w-full rounded-full h-10
-          bg-gray-200 pl-5 ring ring-transparent 
-          focus:ring-green-500 focus:ring-offest-2 
-          transition-shadow placeholder:drop-shadow 
-          invalid:focus:ring-red-500"
-          type="text"
+      flex-col md:flex-row gap-2 *:outline-none
+      ring ring-transparent transition-shadow
+      has-[:invalid]:ring-red-100">
+        <input
+          className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none 
+          ring ring-transparent focus:ring-green-500 focus:ring-offset-2 
+          transition-shadow placeholder:drop-shadow
+           invalid:focus:ring-red-500 peer"
+          type="email"
           required
-          placeholder="Search here..." 
+          placeholder="Email address"
         />
-        <span className="text-red-500 font-medium
-        hidden peer-invalid:block ">Email is required.</span>
-        <button className="text-white py-2
-        rounded-full active:scale-90 focus:scale-90
-        transition-transform font-medium 
-        md:px-10 bg-black ">
+        <span className="text-red-500 font-medium hidden peer-invalid:block ">
+          Email is required.
+        </span>
+        <button className="text-white py-2 rounded-full active:scale-90  
+        transition-transform font-medium outline-none md:px-10 bg-black ">
           Log in
         </button>
       </div>
